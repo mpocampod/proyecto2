cc0ce8376c4578783ca7a3c70811d02a6cb33f0f
mjgutierre <68908889+mjgutierre@users.noreply.github.com>
2023-05-19 13:10:51 -0500

    Create userdata.sh

---
+++ userdata.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/mpocampod/proyecto2.git
+sudo apt update 
+sudo apt install python3-pip
+cd Proyecto2
+pip install -r requirements.txt
+chmod +x userdata.sh
+./userdata.sh
