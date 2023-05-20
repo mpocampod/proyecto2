d2873164f450867069e94ae5e2055be5e8a574e9
jjsanchezc <jjsanchezc@eafit.edu.co>
2023-05-20 15:59:28 -0500

    cambios al sh

--- controllerASG.py
+++ controllerASG.py
@@ -32,9 +32,7 @@ class controllerASG:
     'git clone https://github.com/mpocampod/proyecto2.git',
     'sudo apt update -y',
     'sudo apt install -y python3-pip',
-    'cd proyecto2',
-    'pip install -r requirements.txt',
-    'python3 app/calculadora.py & python3 monitorC.py'
+    
 ]
 
 # Codifica el contenido del script en Base64
