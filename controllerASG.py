bf618e4b5f9d05567c6fb9c5ef582d23380b02bb
jjsanchezc <jjsanchezc@eafit.edu.co>
2023-05-20 15:01:30 -0500

    cambios al sh

--- controllerASG.py
+++ controllerASG.py
@@ -29,13 +29,13 @@ class controllerASG:
             print('Creando Instancia en EC2 ...')
             # Contenido del script que deseas ejecutar
             script_content = """#!/bin/bash
-            git clone https://github.com/mpocampod/proyecto2.git
-            sudo apt update -y
-            sudo apt install -y python3-pip
-            cd proyecto2
-            pip install -r requirements.txt
-            cd app
-            python3 app/calculadora.py & python3 monitorC.py 
+                git clone https://github.com/mpocampod/proyecto2.git
+                sudo apt update -y
+                sudo apt install -y python3-pip
+                cd proyecto2
+                pip install -r requirements.txt
+                cd app
+                python3 app/calculadora.py & python3 monitorC.py 
             """
 
 # Codifica el contenido del script en Base64
